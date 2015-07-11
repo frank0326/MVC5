3b107d48c7fbf9902c0c106d3abe5734edaab2fd
frank0326 <frank0326@msn.com>
2015-07-11 14:20:22 +0800

    新增Index及UpdteFiveCustomerCity方法

--- MVC5/Controllers/HomeController.cs
+++ MVC5/Controllers/HomeController.cs
@@ -8,12 +8,27 @@ namespace MVC5.Controllers
 {
     public class HomeController : Controller
     {
+        FabricsEntities db = new FabricsEntities();
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            var customers = db.Client;
+            return View(customers);
         }
 
 
+        public ActionResult UpdteFiveCustomerCity()
+        {
+            var Top5Customer = db.Client.Take(5);
+
+            foreach (var cust in Top5Customer)
+            {
+                cust.City = "Taipei";
+            }
+
+            db.SaveChanges();
+
+            return View(Top5Customer);
+        }
     }
 }
